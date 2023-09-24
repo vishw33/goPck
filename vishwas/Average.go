e7a357587769c5c8f1a57712436509d88b426e34
vishw33 <vishw33@yahoo.com>
2023-09-24 09:56:46 +0530

    NewPackage

---
+++ vishwas/Average.go
@@ -0,0 +1,9 @@
+package vishwas
+
+func Average(numbers []float64) float64 {
+	sum := 0.0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum / float64(len(numbers))
+}
---
+++ vishwas/go.mod
@@ -0,0 +1,3 @@
+module vishwas
+
+go 1.21.0
